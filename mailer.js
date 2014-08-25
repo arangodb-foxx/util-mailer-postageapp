17da997ed2ed0567fef8312a79882452b8db5eb5
Alan Plum <me@pluma.io>
2014-08-25 15:37:48 +0200

    Return parsed response body. Bump to 1.0.0.

--- mailer.js
+++ mailer.js
@@ -33,6 +33,7 @@ queues.registerJobType(applicationContext.configuration.jobType, {
           body.response.message
         );
       }
+      return body;
     } else if (Math.floor(response.code / 100) !== 2) {
       throw new Error('Server sent an empty response with status ' + response.code);
     }
